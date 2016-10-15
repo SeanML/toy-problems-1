00f3802789e779ecf0327aaa069a7ee3fc983a95
seanml <i.r.sean@gmail.com>
2016-10-14 17:24:09 -0700

    Finished automated grading toy problem

--- challenge/automatedGrading.js
+++ challenge/automatedGrading.js
@@ -9,6 +9,27 @@
 // autograde("Jon 19 14 15 15 16\nJeremy 15 11 10 15 16\nJesse 19 17 20 19 18") => { "all": 15.93, "Jon": 15.8, "Jeremy": 13.4, "Jesse": 18.6 }
 
 // Code:
-function autograde (report) {
-
-}
+const autograde = (report) => {
+  let students = report.split("\n");
+  let result = {};
+  let allGrades = [];
+  result.all = 0;
+  students.forEach((student, index) => {
+    let studentArr = student.split(' ');
+    let currStudent = studentArr[0];
+    result[currStudent] = [];
+    for(let i = 1; i < studentArr.length; i++) {
+      result[currStudent].push(Number(studentArr[i]));
+      allGrades.push(Number(studentArr[i]));
+      if(i === studentArr.length - 1) {
+        result[currStudent] = Number((result[currStudent].reduce((a,b) => {
+          return a + b;
+        }) / result[currStudent].length).toFixed(2));
+      }
+    }
+  });
+  result.all = Number((allGrades.reduce((a,b) => {
+    return a + b;
+  }) / allGrades.length).toFixed(2));
+  return result;
+};
