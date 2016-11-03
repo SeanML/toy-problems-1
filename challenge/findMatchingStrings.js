f0e25b0cf29798acfe0a4c591dd7ee854041ba2f
seanml <i.r.sean@gmail.com>
2016-11-03 16:09:08 -0700

    Finished findMatchingStrings toy problem

---
+++ challenge/findMatchingStrings.js
@@ -0,0 +1,17 @@
+// Given a length (number) and an array of strings, return all strings that are greater than or equal to the given length.
+
+// DO NOT use .filter() in your solution.
+
+// DO use a for loop.
+
+// TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+
+// Code:
+
+const filterLength = (length, array) => {
+  let result = [];
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].length >= length) result.push(array[i]);
+  }
+  return result;
+}
